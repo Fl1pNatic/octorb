0f506988df62b558c1280cf8d7983c0ea9ae4903
Fl1pNatic <newfl1pnatic@gmail.com>
2022-08-11 22:40:08 +0600

    fixed rng1000

--- commands/fun.py
+++ commands/fun.py
@@ -75,8 +75,8 @@ class fun(commands.Cog):
         await ctx.reply("Heads" if result == 1 else "Tails")
 
     @commands.command()
-    async def rng1000(self, ctx, *, args=None):
-        if len(args) < 2 or args == None:
+    async def rng1000(self, ctx, *args):
+        if len(args) < 2:
             await ctx.reply("Input minimum and maximum number (In range of -1000 to 1000)")
             return
 
@@ -88,17 +88,24 @@ class fun(commands.Cog):
 
         min = int(min)
         max = int(max)
-        print(min)
-        print(max)
         if int(min) < -1000 or int(min) > 1000:
             await ctx.reply("Minimum number is too low or high")
             return
         if max < -1000 or max > 1000:
             await ctx.reply("Maximum number is too low or high")    
             return
+        if max < min:
+            await ctx.reply("Maximum number MUST be higher than minimum")
+            return
         
         result = random.randrange(min, max)     
         await ctx.reply(result)
+
+    # @commands.command()
+    async def math(self, ctx, *args):
+            if len(args) < 2 or args == None:
+                await ctx.reply("Input a math action")
+            return
     
     @commands.command()
     async def yo(self, ctx):
--- main.py
+++ main.py
@@ -120,6 +120,7 @@ async def update(ctx:botCommands.Context):
         await ctx.send("You have no perms")
         return
     await gitupdate()
+    ctx.reply("Pulled Changes")
     
 
 
