688e4db9349016257eaa3a90cd37743c9bb875a5
Mattie Nash <mnash@kittymail.com>
2022-08-12 13:41:20 +0000

    XP CALCULATION!!!!

--- commands/xp.py
+++ commands/xp.py
@@ -1,42 +1,45 @@
-from cmath import log
+from math import log
 from concurrent.futures import process
 import random
 import nextcord
 from nextcord.ext import commands
 import asyncio
-messageCounts = {}
 xp = {}
 
 class xp(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.messageCounts = {}
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.processXP()
 
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
-        print(messageCounts)
+        print(self.messageCounts)
         if message.author.bot:
             return
-        if not message.guild.id in messageCounts:
-            messageCounts[message.guild.id] = {}
+        if not message.guild.id in self.messageCounts:
+            self.messageCounts[message.guild.id] = {}
 
-        if not message.author.id in messageCounts[message.guild.id]:
-            messageCounts[message.guild.id][message.author.id] = 1
+        if not message.author.id in self.messageCounts[message.guild.id]:
+            self.messageCounts[message.guild.id][message.author.id] = 1
             return
-        messageCounts[message.guild.id][message.author.id] += 1
-        print(messageCounts[message.guild.id][message.author.id])
+        self.messageCounts[message.guild.id][message.author.id] += 1
 
 
-        async def processXP():
-            while True:
-                await asyncio.sleep(60)
-                await calcXP()
-                messageCounts = {}
+    async def processXP(self):
+        while True:
+            await self.calcXP()
+            await asyncio.sleep(15)
 
-        async def calcXP():
-            for s in messageCounts.keys():
-                for u in messageCounts[s].keys():
-                    uX = log(messageCounts[s][u])
-                    await storeXP(s, u, uX)
+    async def calcXP(self):
+        for s in self.messageCounts.keys():
+            for u in self.messageCounts[s].keys():
+                uX = round(log(self.messageCounts[s][u])+5,0)
+                await self.storeXP(s, u, uX)
+        self.messageCounts = {}
 
-        async def storeXP(serverID, userID, userXP):
-            pass
+    async def storeXP(self, serverID, userID, userXP):
+        pass
