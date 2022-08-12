27686298729301d727228653934d6f54d7987b91
Mattie Nash <mnash@kittymail.com>
2022-08-12 21:35:33 +0000

    discord.py bug fix n. 4

--- commands/xp.py
+++ commands/xp.py
@@ -112,7 +112,7 @@ class xp(commands.Cog):
             cursor.execute("SELECT memberXp FROM xp WHERE serverId = %s and memberId = %s", (ctx.message.guild.id, ctx.message.author.id))
             embed = discord.Embed(title="XP", color=0xff00bb)
             embed.add_field(name=f"{ctx.message.author.display_name}", value=f"`XP: {cursor.fetchone()[0]}`")
-            await ctx.send(str(ctx.message.author.avatar_url) +"?size=1024")
+            print(str(ctx.message.author.avatar_url) +"?size=1024")
             embed.set_thumbnail(str(ctx.message.author.avatar_url) +"?size=1024")
             await ctx.reply(embed=embed)
             return
