4ba09935ba8449c0f97674d30fcc140fcff88f27
Mattie Nash <mnash@kittymail.com>
2022-08-18 17:54:30 +0000

    fix owoify

--- commands/fun.py
+++ commands/fun.py
@@ -52,7 +52,7 @@ class fun(commands.Cog):
     @commands.command(hidden=True)
     async def owoify(self, ctx, *, phrase):
         embed = discord.Embed(title="OwOified")
-        embed.set_author(ctx.author.name)
+        embed.set_author(name=ctx.author.name)
         embed.description = owoify(phrase)
         await ctx.send(embed=embed)
 
