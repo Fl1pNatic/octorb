fa9023cab812e8a1474602ff85886d9b550e41f7
Fl1pNatic <newfl1pnatic@gmail.com>
2022-11-03 04:33:02 +0600

    page count footer

--- commands/xp.py
+++ commands/xp.py
@@ -134,7 +134,7 @@ class xp(commands.Cog):
         for i in range(min(len(data) + page, 5)):
             embed.add_field(
                 name=f"{(i + 1 + (5 * (page - 1)))}.", value=f"<@{data[i + (5 * (page - 1))][1]}>: `{data[i + (5 * (page - 1))][0]}`", inline=False)
-
+        embed.set_footer("Page: " + page)
         await ctx.reply(embed=embed)
 
     @commands.hybrid_command(description="Gives the specified user xp.")
